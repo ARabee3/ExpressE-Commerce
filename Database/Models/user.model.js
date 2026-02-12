51b08af662ad93650c9142c72cbab07da0d8d480
Ahmed Rabie <arabeea7104@gmail.com>
2026-02-12 20:14:52 +0200

    Add User Model

---
+++ Database/Models/user.model.js
@@ -0,0 +1,49 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["Customer", "Admin", "Seller"],
+      default: "Customer",
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+    wishlist: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Product",
+      },
+    ],
+  },
+  { timestamps: true },
+);
+
+export const userModel = mongoose.model("User", userSchema);
