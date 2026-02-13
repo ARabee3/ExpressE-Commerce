84d12e8f1068a714f78af506ef6164920464cf48
Mostafa Ahmed <modrsh2002@gmail.com>
2026-02-13 20:53:22 +0200

    add crud oeration for order and handle it in order routes but hashed it untill other model finshed

--- Modules/Order/order.routes.js
+++ Modules/Order/order.routes.js
@@ -1,14 +1,27 @@
+import express from "express";
+import { validateData } from "../../Middlewares/validationDate.js";
+import { orderModel } from "../../Database/Models/order.model.js";
+import { orderValidation } from "../../Validations/orderValidation.js";
+import { isAuthor } from "../../Middlewares/isAuthor.js";
+// import {
+//   addOrder,
+//   getUserOrders,
+//   getOrderById,
+//   cancelOrder,
+//   updatePaidStatus,
+// } from "./order.controller.js";
 
-import express from "express"
-import { validateData } from "../../Middlewares/validationDate.js"
-import { orderModel } from "../../Database/Models/order.model.js"
-import { addOrder } from "./order.controller.js"
-import { orderValidation } from "../../Validations/orderValidation.js"
+const orderRoutes = express.Router();
 
-
-const orderRoutes = express.Router()
-
-orderRoutes.post("/orders",validateData(orderValidation),addOrder)
-
+// orderRoutes.get("/orders", getUserOrders);
+// orderRoutes.get("/orders/:id", getOrderById);
+// orderRoutes.post(
+//   "/orders",
+//   validateData(orderValidation),
+//   isAuthor(cartModel, "cart"),
+//   addOrder,
+// );
+// orderRoutes.put("orders/:id/cancel", cancelOrder);
+// orderRoutes.put("/orders/:id/pay", updatePaidStatus);
 
 export default orderRoutes;
