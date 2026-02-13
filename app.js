b39273d2539e53581919efb8d689176fecae322b
Sohyla Gomaa <sohylaelqourashi@gmail.com>
2026-02-13 19:56:49 +0200

    Add category model(schema),validation on attribute,basic CRUD(create and get all categories)

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ import { dbConnection } from "./Database/dbConnection.js";
 import { globalErrorHandler } from "./Middlewares/globalErrorHandler.js";
 import { orderModel } from "./Database/Models/order.model.js";
 import orderRoutes from "./Modules/Order/order.routes.js";
+import { categoryModel } from "./Database/Models/category.model.js";
+import categoryRoutes from "./Modules/Category/category.routes.js";
+
 dbConnection();
 const app = express();
 
@@ -12,6 +15,9 @@ app.use(express.json());
 app.use(globalErrorHandler);
 
 orderModel;
+categoryModel;
+
+app.use(categoryRoutes)
 app.use(orderRoutes)
 app.listen(3000, () => {
   console.log("Server is running successfully at port 3000");
