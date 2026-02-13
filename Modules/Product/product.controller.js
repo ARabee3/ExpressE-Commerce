00c9274212fae9176f20d5a80f755acc46e4af0d
Ahmed Rabie <arabeea7104@gmail.com>
2026-02-14 00:50:01 +0200

    adjusting path

--- Modules/Product/product.controller.js
+++ Modules/Product/product.controller.js
@@ -1,22 +1,22 @@
 import { populate } from "dotenv";
 import { productModel } from "../../Database/Models/product.model.js";
-import { catchAsync } from "../../Utils/catchAsync.js";
+import { catchAsync } from "../../Utils/Error/catchAsync.js";
 
-export const createProduct = catchAsync(async(req,res,next)=>{
-    const product  = await productModel.insertOne(req.body);
+export const createProduct = catchAsync(async (req, res, next) => {
+  const product = await productModel.insertOne(req.body);
 
-    res.status(201).json({
-        status: "success",
-        data: product
-    });
+  res.status(201).json({
+    status: "success",
+    data: product,
+  });
 });
 
-export const getProducts = catchAsync(async (req, res,next) => {
-    const products = await productModel.find().populate("category");
+export const getProducts = catchAsync(async (req, res, next) => {
+  const products = await productModel.find().populate("category");
 
-    res.status(200).json({
-        status: "success",
-        results: products.length,
-        data: products
-    });
+  res.status(200).json({
+    status: "success",
+    results: products.length,
+    data: products,
+  });
 });
