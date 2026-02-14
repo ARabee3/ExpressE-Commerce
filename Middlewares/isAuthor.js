bcc216ff2cd7afac215c31d45b575b05798fcb79
Mostafa Ahmed <modrsh2002@gmail.com>
2026-02-14 15:43:51 +0200

    un comment to order logic in controller and routes

--- Middlewares/isAuthor.js
+++ Middlewares/isAuthor.js
@@ -1,27 +1,27 @@
 import { AppError } from "../Utils/Error/AppError.js";
-import { orderModel } from "../Database/Models/order.model.js";
+
 
 export const isAuthor = (model, entity) => {
   return async (req, res, next) => {
     let entityId = req.params.id; // if called from crud ops of oreder by id of order => in params
-    // if (entity === "cart") {
-    //   entityId = req.body.cartId;
-    //   // as if cart model => it be called from addOrder method to check cart belong to user or not
-    //   //and cart id will be req.body not params
-    // }
-    // const id = req.user._id; // will get from verify token middleware
+    if (entity === "cart") {
+      entityId = req.body.cartId;
+      // as if cart model => it be called from addOrder method to check cart belong to user or not
+      //and cart id will be req.body not params
+    }
+    const id = req.user._id; // will get from verify token middleware
 
-    // const order = await model.findOne({ _id: entityId, userId: id });
+    const order = await model.findOne({ _id: entityId, userId: id });
 
-    // // console.log(entity);
-    // if (!order) {
-    //   return next(
-    //     new AppError(
-    //       `You are not the author of this ${entity} or it does not exist`,
-    //       403,
-    //     ),
-    //   );
-    // }
+    // console.log(entity);
+    if (!order) {
+      return next(
+        new AppError(
+          `You are not the author of this ${entity} or it does not exist`,
+          403,
+        ),
+      );
+    }
     next();
   };
 };
--- Modules/Order/order.routes.js
+++ Modules/Order/order.routes.js
@@ -3,25 +3,26 @@ import { validateData } from "../../Middlewares/validationDate.js";
 import { orderModel } from "../../Database/Models/order.model.js";
 import { orderValidation } from "../../Validations/orderValidation.js";
 import { isAuthor } from "../../Middlewares/isAuthor.js";
-// import {
-//   addOrder,
-//   getUserOrders,
-//   getOrderById,
-//   cancelOrder,
-//   updatePaidStatus,
-// } from "./order.controller.js";
+import { cartModel } from "../../Database/Models/cart.model.js";
+import {
+  addOrder,
+  getUserOrders,
+  getOrderById,
+  cancelOrder,
+  updatePaidStatus,
+} from "./order.controller.js";
 
 const orderRoutes = express.Router();
 
-// orderRoutes.get("/orders", getUserOrders);
-// orderRoutes.get("/orders/:id",isAuthor(orderModel, "order"), getOrderById);
-// orderRoutes.post(
-//   "/orders",
-//   validateData(orderValidation),
-//   isAuthor(cartModel, "cart"),
-//   addOrder,
-// );
-// orderRoutes.put("orders/:id/cancel", cancelOrder);
-// orderRoutes.put("/orders/:id/pay", updatePaidStatus);
+orderRoutes.get("/orders", getUserOrders);
+orderRoutes.get("/orders/:id",isAuthor(orderModel, "order"), getOrderById);
+orderRoutes.post(
+  "/orders",
+  validateData(orderValidation),
+  isAuthor(cartModel, "cart"),
+  addOrder,
+);
+orderRoutes.put("orders/:id/cancel", cancelOrder);
+orderRoutes.put("/orders/:id/pay", updatePaidStatus);
 
 export default orderRoutes;
