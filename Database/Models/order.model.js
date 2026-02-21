f8fda54746dcdcb99e3dc26fba3042f126b007cb
jowael <yousefwail22@gmail.com>
2026-02-21 17:11:09 +0200

    feat(order): add Order model and schema with status tracking

---
+++ Database/Models/order.model.js
@@ -0,0 +1,95 @@
+import mongoose, { Schema } from "mongoose";
+
+const orderSchema = new Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    cartId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Cart",
+      required: true,
+    },
+    shippingAddress: {
+      street: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      state: {
+        type: String,
+        required: true,
+      },
+    },
+    orderItems: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        productTitle: {
+          type: String,
+          required: true,
+        },
+        productImg: String,
+        price: {
+          type: Number,
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+      },
+    ],
+    totalOrderPrice: {
+      type: Number,
+      required: true,
+    },
+    discountAmount: Number,
+    finalPrice: Number,
+    paymentMethod: {
+      type: String,
+      enum: ["Card", "Cash", "Wallet"],
+      required: true,
+    },
+    isPaid: {
+      type: Boolean,
+      default: false,
+    },
+    paidAt: Date,
+    status: {
+      type: String,
+      enum: ["Pending", "Processing", "Shipped", "Delivered", "Cancelled"],
+      default: "Pending",
+    },
+    couponId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Coupon",
+    },
+    deliveredAt: Date,
+    cancelledAt: Date,
+    processedAt: Date,
+    shippedAt: Date,
+  },
+  {
+    timestamps: true,
+  },
+);
+
+orderSchema.index({
+  createdAt: -1,
+});
+
+orderSchema.index({
+  status: 1,
+});
+
+export const orderModel = mongoose.model("Order", orderSchema);
