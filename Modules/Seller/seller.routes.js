b958126fef27f298063e1f6b7816cb5288cea9ba
Mostafa Ahmed <modrsh2002@gmail.com>
2026-02-23 00:06:04 +0200

    add seller validation

--- Modules/Seller/seller.routes.js
+++ Modules/Seller/seller.routes.js
@@ -2,10 +2,12 @@ import { getMyProducts, registerAsSeller } from "./seller.controller.js";
 import { Router } from "express";
 import { isSellerAndActive } from "../../Middlewares/isSellerAndActive.js";
 import { verifyToken } from "../../Middlewares/verifyToken.js";
+import { validate } from "../../Middlewares/validate.js";
+import { sellerValidation } from "../../Validations/sellerValidation.js";
 
 const sellerRoutes= Router();
 
 sellerRoutes.get("/seller/my-products",verifyToken,isSellerAndActive,getMyProducts);
-sellerRoutes.post("/seller/register-as-seller",verifyToken,registerAsSeller);
+sellerRoutes.post("/seller/register-as-seller",verifyToken,validate(sellerValidation),registerAsSeller);
 
 export default sellerRoutes;
